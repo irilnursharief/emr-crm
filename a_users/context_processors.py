6cb28c863966e73939206457d304d0f08e3da382
irilnursharief <iril.nursharief@gmail.com>
2026-04-06 16:58:06 +0800

    feat: Enhance navigation and repair functionality with URL handling and form updates

--- a_users/context_processors.py
+++ a_users/context_processors.py
@@ -1,6 +1,15 @@
-from django.urls import resolve
+from django.urls import resolve, Resolver404
 
 
 def current_url_name(request):
-    match = resolve(request.path_info)
-    return {"current_url_name": match.url_name, "current_app": match.app_name}
+    try:
+        match = resolve(request.path_info)
+        return {
+            "current_url_name": match.url_name,
+            "current_app": match.app_name,
+        }
+    except Resolver404:
+        return {
+            "current_url_name": None,
+            "current_app": None,
+        }
